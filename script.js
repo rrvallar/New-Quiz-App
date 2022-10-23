96c8b58a75af389422928751ddd1b3ebf5cbb6f0
rrvallar <rrvallar@gmail.com>
2022-10-23 14:10:01 -0400

    quiz-test

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // window.alert("Hello");
 
-let score = 0
+// let score = 0
+// for(var i=0; i < questions.length; i++) {
 
-let questions = [{
+// }
 
+// let questions = [{
 
-prompt: "How does a for loop start?"
-answer: "b"
-}
-]
+// prompt: "How does a for loop start?"
+// answer: "b"
+// }
+// ]
