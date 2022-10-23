13a4f6ba34f5a83c649eeeb9368b8f9529ee4e54
rrvallar <rrvallar@gmail.com>
2022-10-23 18:35:35 -0400

    final

--- script.js
+++ script.js
@@ -11,3 +11,29 @@
 // answer: "b"
 // }
 // ]
+// document.getElementById
+document.getElementsByClassName("box")[0].style.display = "block";
+function next(id) {
+  document.getElementsByClassName("box")[id - 1].style.display = "none";
+  document.getElementsByClassName("box")[id].style.display = "block";
+}
+
+function results() {
+  let result = 0;
+  if (document.getElementById(rightAnswer1).checked) {
+    result++;
+  }
+  if (document.getElementById(rightAnswer2).checked) {
+    result++;
+  }
+  if (document.getElementById(rightAnswer3).checked) {
+    result++;
+  }
+  if (document.getElementById(rightAnswer4).checked) {
+    result++;
+  }
+  if (document.getElementById(rightAnswer5).checked) {
+    result++;
+  }
+  window.alert("FINAL SCORE:" + score);
+}
